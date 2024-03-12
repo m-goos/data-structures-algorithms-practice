5105b6c5f4293de1c79d0315dec553367cd5a659
m-goos <10080192+m-goos@users.noreply.github.com>
2024-03-12 15:45:58 +0100

    docs: match formatting with other solutions

--- codility/solutions/frogJump2.ts
+++ codility/solutions/frogJump2.ts
@@ -1,18 +1,17 @@
+/**
+ * Approach
+ * distanceToGo = Y - X
+ *
+ * divide distance by jumping distance, this is the number of jumps
+ * if it has a decimal, round up (add 1 jump, because we cannot jump half a distance)
+ *
+ * Rounding up can be done in multiple ways, e.g.:
+ * - using the remainder operator (`%`)
+ * - using the built-in Math.ceil
+ *
+ * @SEE https://app.codility.com/programmers/lessons/3-time_complexity/frog_jmp/
+ */
 function frogJump2(X: number, Y: number, D: number): number {
-  /**
-   * Approach
-   * distanceToGo = Y - X
-   *
-   * divide distance by jumping distance, this is the number of jumps
-   * if it has a decimal, round up (add 1 jump, because we cannot jump half a distance)
-   *
-   * Rounding up can be done in multiple ways, e.g.:
-   * - using the remainder operator (`%`)
-   * - using the built-in Math.ceil
-   *
-   * @SEE https://app.codility.com/programmers/lessons/3-time_complexity/frog_jmp/
-   */
-
   const distanceToGo = Y - X;
 
   // edge case: frog is already on the other side of the road
